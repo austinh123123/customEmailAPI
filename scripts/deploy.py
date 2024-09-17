26ddb038cc5602c28a6cdbb3949fd1fec6592d63
austinh123123 <88354330+austinh123123@users.noreply.github.com>
2024-09-17 10:05:18 -0700

    Fixed policy upload out of order

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -22,8 +22,47 @@ def get_token(tenant: str, clientId: str, clientSecret: str, **kwargs):
 def upload_endpoint(id):
     return f"https://graph.microsoft.com/beta/trustFramework/policies/{id}/$value"
 
+def upload_policies(policyFolder: str, token: str):
+    policyTree = {}
+    base_policy = None 
+    base_policy_text = None
+    base_policy_root = None
+    for policy in policyUtils.get_policy_paths(policyFolder):
+        with open(policy, "r") as f:
+            try:  
+                root = policyUtils.parse_policy(policy)
+                id = policyUtils.getId(root)
+                base_id = policyUtils.get_basePolicy_id(root)
+                if base_id is not None:
+                    policyTree[base_id] = {"id": id, "text": f.read(), "root": root}
+                else:
+                    base_policy = id
+                    base_policy_text = f.read()
+                    base_policy_root = root
+            except:
+                print(f"Could not parse file: {policy}, skipping upload")
 
-def upload(policy_id: str, policy_text: str, token:str):
+    def upload_tree(id: str, text:str, root, tree):
+        upload_policy(id, text, token)
+        print(f"Uploaded policy {id} to {token_info['tenant']}")
+        if policyUtils.is_RP_file(root):
+            print(f"Run now link: {run_now_url(id)}")
+        if id in tree.keys():
+            new_id = tree[id]["id"]
+            new_text = tree[id]["text"]
+            new_root = tree[id]["root"]
+            upload_tree(new_id, new_text, new_root, tree)
+
+
+    assert base_policy is not None, "Could not find a base policy! Aborting upload"
+    upload_tree(
+        base_policy, 
+        base_policy_text, 
+        base_policy_root, 
+        policyTree
+    )
+
+def upload_policy(policy_id: str, policy_text: str, token:str):
     endpoint = upload_endpoint(policy_id)
     headers = {
         "Content-Type": f"xml; charset=utf-8",
@@ -50,17 +89,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
     with open(args.tenant, "r") as f: 
         token_info = json.loads(f.read())
-
-
     token = get_token(**token_info)
-    for policy in policyUtils.get_policy_paths(args.policyFolder):
-        with open(policy, "r") as f:
-            try:  
-                root = policyUtils.parse_policy(policy)
-                id = policyUtils.getId(root)
-                upload(id, f.read(), token)
-                print(f"Uploaded policy {id} to {token_info['tenant']}")
-                if policyUtils.is_RP_file(root):
-                    print(f"Run now link: {run_now_url(id)}")
-            except:
-                print(f"Could not parse file: {policy}, skipping upload")
+    upload_policies(args.policyFolder, token)
